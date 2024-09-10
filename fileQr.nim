aea00c7097d5b0b17272377e6b66d1515f6925a3
vert2 <vert2air@gmail.com>
2024-09-10 21:02:02 +0900

    snapshot : 1st commit
    Initial commit
    
    Changes to be committed:
    	new file:   fileQr.nim

---
+++ fileQr.nim
@@ -0,0 +1,172 @@
+import wNim/wNim/[wApp, wBitmap, wButton, wComboBox, wFileDialog, wFrame, wImage, wNoteBook,
+    wPanel, wRadioButton, wStaticBitmap, wStaticBox, wStaticText, wStatusBar, wTextCtrl, ]
+import qr
+import std/strformat
+import strutils
+    # wMemoryDC,
+
+let app = App(wSystemDpiAware)
+let frame = Frame(title="File <=> QR")
+let panel = Panel(frame)
+let panel_qr = NoteBook(panel)
+panel_qr.addPage("QR code")
+
+proc num2bin2(number: int): string =
+    var buffer: array[2, string]
+    buffer[0] = fmt"{char((number shr  0) and 0xff)}"
+    buffer[1] = fmt"{char((number shr  8) and 0xff)}"
+    return buffer.join()
+
+proc num2bin4(number: int): string =
+    var buffer: array[4, string]
+    buffer[0] = fmt"{char((number shr  0) and 0xff)}"
+    buffer[1] = fmt"{char((number shr  8) and 0xff)}"
+    buffer[2] = fmt"{char((number shr 16) and 0xff)}"
+    buffer[3] = fmt"{char((number shr 24) and 0xff)}"
+    return buffer.join()
+
+proc make_qr_data(data: string, err_cor: enum): seq[string] =
+    result = @[]
+    for line in qrBinary(data).splitLines:   # , eccLevel=Ecc_Low)
+        if line.len > 0:
+            result.insert(line, 0)
+
+proc qr_data_to_pbm(pbm_fn: string, qrbin: string) =
+    let line_len = qrbin.find("\n")
+    block:
+        var f: File = open(pbm_fn, FileMode.fmWrite)
+        defer:
+            close(f)
+        f.write("P1\n")
+        f.write(fmt"{line_len} {line_len}" & "\n")
+        f.write(qrbin)
+
+proc qr_data_to_bmp(bmp_fn: string, qrbin: seq[string]) =
+    # BMPファイルフォーマット
+    # https://www.setsuki.com/hsp/ext/bmp.htm
+    let line_len = qrbin.len
+
+    block:
+        # var stream = newFileStream(bmp_fn, FileMode.fmWrite)
+        var f: File = open(bmp_fn, FileMode.fmWrite)
+        defer:
+            f.close()
+        f.write('B') # bfType
+        f.write('M') # bfType
+        let data_size: int = ((3 * line_len + 3) div 4) * 4 * line_len
+        f.write(num2bin4(14 + 40 + data_size)) # bfSize
+        f.write(num2bin2(0)) # bfReserved1
+        f.write(num2bin2(0)) # bfReserved2
+        f.write(num2bin4(14 + 40)) # bfOffBits
+
+        f.write(num2bin4(40)) # bcSize
+        f.write(num2bin4(line_len)) # bcWidth
+        f.write(num2bin4(line_len)) # bcHeight
+        f.write(num2bin2(1)) # bcPlanes
+        f.write(num2bin2(24)) # bcBitCount
+
+        f.write(num2bin4(0)) # biComression 0:BI_RGB
+        f.write(num2bin4(data_size)) # biSizeImage
+        f.write(num2bin4(0)) # biXPixPerMeter
+        f.write(num2bin4(0)) # biYPixPerMeter
+        f.write(num2bin4(0)) # biClrUsed
+        f.write(num2bin4(0)) # biClrImportant
+
+        for line in qrbin:
+            for ch in line:
+                case ch:
+                of '0': f.write(fmt"{char(0xff)}{char(0xff)}{char(0xff)}")
+                of '1': f.write(fmt"{char(0x00)}{char(0x00)}{char(0x00)}")
+                else:
+                    echo "skip 1 char"
+            for ix in (3 * line_len mod 4) ..< 4:
+                f.write(fmt"{char(0x00)}")
+
+var bm = StaticBitmap(panel_qr.page(0), bitmap=Bitmap("test.bmp"), style=wSbFit)
+
+proc displayQR(bmp_fn: string) =
+    bm = StaticBitmap(panel_qr.page(0), bitmap=Bitmap(bmp_fn), style=wSbFit)
+    bm.backgroundColor = -1
+    #var memDc = MemoryDC()
+    #var img = images[0]
+    #memDc.clear()
+    #memDc.drawImage(img, x=0, y=0)
+    panel.refresh()
+
+    
+
+frame.dpiAutoScale:
+    frame.size = (750, 450)
+#panel_qr.dpiAutoScale:
+#    panel_qr.size = (350, 250)
+
+let statusBar = StatusBar(frame)
+
+let box_input  = StaticBox(panel, label="Select Input")
+let box_QR     = StaticBox(panel, label="Display QR code")
+let box_decode = StaticBox(panel, label="Decode file")
+let box_output = StaticBox(panel, label="Output")
+# box_input.margin = 20
+# box_QR.margin = 20
+# box_decode.margin = 20
+box_output.margin = 20
+
+let rb_file = RadioButton(panel, label="File Name")
+let rb_text = RadioButton(panel, label="Direct Text")
+
+let input_file= TextCtrl(panel)
+let btn_input = Button(panel, label="Open..")
+let file_sel = FileDialog(panel, message="Open...")
+
+let input_text = TextCtrl(panel)
+
+let label_err = StaticText(panel, label="Error correct")
+let list_err = ["L(7%)", "M(15%)", "Q(25%)", "H(30%)"]
+let cb_err = ComboBox(panel, value=list_err[0], choices=list_err)
+let btn_qr = Button(panel, label="Display QR codes")
+
+let btn_decode = Button(panel, label="Output Decode file")
+
+proc layout() =
+    panel.autolayout """
+        H:|-40-[box_input]-40-|
+        H:|-40-[box_QR]-[box_decode]-40-|
+        V:|-[box_input]-30-[box_QR,box_decode]-40-|
+
+        outer: box_input
+        H:|-[rb_file]-[input_file]-[btn_input(75)]-|
+        H:|-[rb_text]-[input_text]-|
+        V:|-[rb_file,input_file,btn_input]-[rb_text, input_text]-|
+
+        outer: box_QR
+        H:|-[label_err]-[cb_err]-|
+        H:|-[btn_qr]-|
+        H:|-[panel_qr]-|
+        V:|-[panel_qr]-[label_err,cb_err]-[btn_qr]-|
+
+        outer: box_decode
+        H:|-[btn_decode]-|
+        V:|-[btn_decode]-|
+    """
+    # V:|-[label_err,cb_err]-[btn_qr]-[panel_qr]-|
+    panel_qr.autolayout """
+        H:|-[bm]-|
+        V:|-[bm]-|
+    """
+    box_output.contain(box_QR, box_decode)
+
+btn_input.wEvent_Button do ():
+    let input_file_name: seq[string] = file_sel.display()
+    input_file.setValue(input_file_name[0])
+
+btn_qr.wEvent_Button do ():
+    let a = make_qr_data("abc", Ecc_Low)
+    echo a
+    qr_data_to_bmp("test.bmp", a)
+    displayQR("test.bmp")
+
+layout()
+frame.center()
+frame.show()
+app.mainLoop()
+        
